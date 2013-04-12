f37d4092e11e1bae00655d7d117f3c5c92941b81
Mustaqim Moulavi <bestmust@gmail.com>
2013-04-12 00:55:07 -0400

    Wrote GroupBy::Operation. Haven't yet tested it. formatted relop.*

--- project3/RelOp.cc
+++ project3/RelOp.cc
@@ -2,118 +2,116 @@
 #include "BigQ.h"
 
 void* SF_Thread(void *sf_currentObj) {
-    
-        SelectFile obj = *((SelectFile *) sf_currentObj);
 
-        obj.Operation();
-        pthread_exit(NULL);
-        //Peform a getnext to fetch the record from dbfile and keep a counter that increments when a page overflow
+    SelectFile obj = *((SelectFile *) sf_currentObj);
+
+    obj.Operation();
+    pthread_exit(NULL);
+    //Peform a getnext to fetch the record from dbfile and keep a counter that increments when a page overflow
 }
 
-void SelectFile::Run (DBFile &inFile, Pipe &outPipe, CNF &selOp, Record &literal) {
+void SelectFile::Run(DBFile &inFile, Pipe &outPipe, CNF &selOp, Record &literal) {
     this->sf_inputFile = &inFile;
     this->sf_outputPipe = &outPipe;
     this->sf_selectOperator = &selOp;
-    this->sf_literalRecord = &literal;   
-    
-    int pthreadvar = pthread_create(&thread, NULL, &SF_Thread,(void *) this);
+    this->sf_literalRecord = &literal;
+
+    int pthreadvar = pthread_create(&thread, NULL, &SF_Thread, (void *) this);
     if (pthreadvar) {
-                printf("Error while creating a thread\n");
-                exit(-1);
-        }
-    
+        printf("Error while creating a thread\n");
+        exit(-1);
+    }
+
 }
 
-void SelectFile::Operation () {
-	
+void SelectFile::Operation() {
+
     Record tempRec;
-    
-    while(sf_inputFile->GetNext(tempRec,*sf_selectOperator,*sf_literalRecord)) {
+
+    while (sf_inputFile->GetNext(tempRec, *sf_selectOperator, *sf_literalRecord)) {
         sf_outputPipe->Insert(&tempRec);
-        }
-    
+    }
+
     sf_outputPipe->ShutDown();
-    
+
 }
 
-void SelectFile::WaitUntilDone () {
-	pthread_join (thread, NULL);
+void SelectFile::WaitUntilDone() {
+    pthread_join(thread, NULL);
 }
 
-void SelectFile::Use_n_Pages (int runlen) {
+void SelectFile::Use_n_Pages(int runlen) {
     runLength = runlen;
 }
 
 void* P_Thread(void *p_currentObj) {
-    
-        Project obj = *((Project *) p_currentObj);
 
-        obj.Operation();
-        pthread_exit(NULL);
-        //Peform a getnext to fetch the record from dbfile and keep a counter that increments when a page overflow
+    Project obj = *((Project *) p_currentObj);
+
+    obj.Operation();
+    pthread_exit(NULL);
+    //Peform a getnext to fetch the record from dbfile and keep a counter that increments when a page overflow
 }
 
-void Project::Run (Pipe &inPipe, Pipe &outPipe, int *keepMe, int numAttsInput, int numAttsOutput) {
+void Project::Run(Pipe &inPipe, Pipe &outPipe, int *keepMe, int numAttsInput, int numAttsOutput) {
     this->p_inPipe = &inPipe;
     this->p_outPipe = &outPipe;
     this->p_keepMe = keepMe;
     this->p_numAttsInput = numAttsInput;
     this->p_numAttsOutput = numAttsOutput;
-    
-    int pthreadvar = pthread_create(&thread, NULL, &P_Thread,(void *) this);
+
+    int pthreadvar = pthread_create(&thread, NULL, &P_Thread, (void *) this);
     if (pthreadvar) {
-                printf("Error while creating a thread\n");
-                exit(-1);
-        }
-    
+        printf("Error while creating a thread\n");
+        exit(-1);
+    }
+
 }
 
-void Project::Operation () {
+void Project::Operation() {
     Record tempRec;
-    while(p_inPipe->Remove(&tempRec))
-    {
-        tempRec.Project(p_keepMe,p_numAttsOutput,p_numAttsInput);
+    while (p_inPipe->Remove(&tempRec)) {
+        tempRec.Project(p_keepMe, p_numAttsOutput, p_numAttsInput);
         p_outPipe->Insert(&tempRec);
     }
     p_outPipe->ShutDown();
 }
 
-void Project::WaitUntilDone () {
-	pthread_join (thread, NULL);
+void Project::WaitUntilDone() {
+    pthread_join(thread, NULL);
 }
 
-void Project::Use_n_Pages (int runlen) {
+void Project::Use_n_Pages(int runlen) {
     runLength = runlen;
 }
 
-
 void* DR_Thread(void *dr_currentObj) {
-    
-        DuplicateRemoval obj = *((DuplicateRemoval *) dr_currentObj);
 
-        obj.Operation();
-        pthread_exit(NULL);
-        //Peform a getnext to fetch the record from dbfile and keep a counter that increments when a page overflow
+    DuplicateRemoval obj = *((DuplicateRemoval *) dr_currentObj);
+
+    obj.Operation();
+    pthread_exit(NULL);
+    //Peform a getnext to fetch the record from dbfile and keep a counter that increments when a page overflow
 }
 
-void DuplicateRemoval::Run (Pipe &inPipe, Pipe &outPipe, Schema &mySchema) {
+void DuplicateRemoval::Run(Pipe &inPipe, Pipe &outPipe, Schema &mySchema) {
     this->dr_inPipe = &inPipe;
     this->dr_outPipe = &outPipe;
     this->dr_mySchema = &mySchema;
-    
-    int pthreadvar = pthread_create(&thread, NULL, &DR_Thread,(void *) this);
+
+    int pthreadvar = pthread_create(&thread, NULL, &DR_Thread, (void *) this);
     if (pthreadvar) {
-                printf("Error while creating a thread\n");
-                exit(-1);
-        }
-    
+        printf("Error while creating a thread\n");
+        exit(-1);
+    }
+
 }
 
-void DuplicateRemoval::Operation () {
-    
+void DuplicateRemoval::Operation() {
+
     OrderMaker dr_orderMaker(dr_mySchema);
     Pipe sortedOut(PIPE_BUFF_SIZE);
-    
+
     //the code in the comment is to create the BigQ thread. but now it is not needed.
     /*
     BigQarg arg={*dr_inPipe,sortedOut,dr_orderMaker,runLength};
@@ -127,233 +125,271 @@ void DuplicateRemoval::Operation () {
             cout<<"\n Error while creating the BigQ Thread error# ";
         }
      */
-    BigQ bq(*dr_inPipe,sortedOut,dr_orderMaker,runLength);
-    
-    Record rec,recNext;
+    BigQ bq(*dr_inPipe, sortedOut, dr_orderMaker, runLength);
+
+    Record rec, recNext;
     ComparisonEngine comp;
-    
-    if(sortedOut.Remove(&rec))
-    {
-        while(sortedOut.Remove(&recNext))
-        {
-            if(comp.Compare(&rec,&recNext,&dr_orderMaker)!=0)
-            {
+
+    if (sortedOut.Remove(&rec)) {
+        while (sortedOut.Remove(&recNext)) {
+            if (comp.Compare(&rec, &recNext, &dr_orderMaker) != 0) {
                 dr_outPipe->Insert(&rec);
-                rec=recNext;
+                rec = recNext;
             }
         }
         dr_outPipe->Insert(&rec);
     }
-    
+
     dr_outPipe->ShutDown();
-    
+
 }
 
-void DuplicateRemoval::WaitUntilDone () {
-	pthread_join (thread, NULL);
+void DuplicateRemoval::WaitUntilDone() {
+    pthread_join(thread, NULL);
 }
 
-void DuplicateRemoval::Use_n_Pages (int runlen) {
+void DuplicateRemoval::Use_n_Pages(int runlen) {
     runLength = runlen;
 }
 
-
 void* WO_Thread(void *wo_currentObj) {
-    
-        WriteOut obj = *((WriteOut *) wo_currentObj);
 
-        obj.Operation();
-        pthread_exit(NULL);
-        //Peform a getnext to fetch the record from dbfile and keep a counter that increments when a page overflow
+    WriteOut obj = *((WriteOut *) wo_currentObj);
+
+    obj.Operation();
+    pthread_exit(NULL);
+    //Peform a getnext to fetch the record from dbfile and keep a counter that increments when a page overflow
 }
 
-void WriteOut::Run (Pipe &inPipe, FILE *outFile, Schema &mySchema) {
+void WriteOut::Run(Pipe &inPipe, FILE *outFile, Schema &mySchema) {
     wo_inPipe = &inPipe;
     wo_outFile = outFile;
     wo_mySchema = &mySchema;
-    
-    int pthreadvar = pthread_create(&thread, NULL, &WO_Thread,(void *) this);
+
+    int pthreadvar = pthread_create(&thread, NULL, &WO_Thread, (void *) this);
     if (pthreadvar) {
-                printf("Error while creating a thread\n");
-                exit(-1);
-        }
-    
-}
-
-void WriteOut::Operation () {
-    
-    Record rec;
-    
-    while(wo_inPipe->Remove(&rec))
-    {
-        rec.PrintToFile(wo_outFile,wo_mySchema);
+        printf("Error while creating a thread\n");
+        exit(-1);
     }
-    
+
 }
 
-void WriteOut::WaitUntilDone () {
-	pthread_join (thread, NULL);
+void WriteOut::Operation() {
+
+    Record rec;
+
+    while (wo_inPipe->Remove(&rec)) {
+        rec.PrintToFile(wo_outFile, wo_mySchema);
+    }
+
 }
 
-void WriteOut::Use_n_Pages (int runlen) {
+void WriteOut::WaitUntilDone() {
+    pthread_join(thread, NULL);
+}
+
+void WriteOut::Use_n_Pages(int runlen) {
     runLength = runlen;
 }
 
 void* SP_Thread(void *sp_currentObj) {
-    
-        SelectPipe obj = *((SelectPipe *) sp_currentObj);
 
-        obj.Operation();
-        pthread_exit(NULL);
+    SelectPipe obj = *((SelectPipe *) sp_currentObj);
+
+    obj.Operation();
+    pthread_exit(NULL);
 }
 
-void SelectPipe::Run (Pipe &inPipe, Pipe &outPipe, CNF &selOp, Record &literal) {
+void SelectPipe::Run(Pipe &inPipe, Pipe &outPipe, CNF &selOp, Record &literal) {
     sp_inPipe = &inPipe;
     sp_outPipe = &outPipe;
     sp_selOp = &selOp;
     sp_literal = &literal;
-    
-    int pthreadvar = pthread_create(&thread, NULL, &SP_Thread,(void *) this);
+
+    int pthreadvar = pthread_create(&thread, NULL, &SP_Thread, (void *) this);
     if (pthreadvar) {
-                printf("Error while creating a thread\n");
-                exit(-1);
+        printf("Error while creating a thread\n");
+        exit(-1);
     }
-    
+
 }
 
-void SelectPipe::Operation () {
+void SelectPipe::Operation() {
     Record rec;
     ComparisonEngine comp;
-    
-    while(sp_inPipe->Remove(&rec)) {
-        if(comp.Compare(&rec,sp_literal,sp_selOp))
+
+    while (sp_inPipe->Remove(&rec)) {
+        if (comp.Compare(&rec, sp_literal, sp_selOp))
             sp_outPipe->Insert(&rec);
     }
     sp_outPipe->ShutDown();
 }
 
-void SelectPipe::WaitUntilDone () {
-	pthread_join (thread, NULL);
+void SelectPipe::WaitUntilDone() {
+    pthread_join(thread, NULL);
 }
 
-void SelectPipe::Use_n_Pages (int runlen) {
+void SelectPipe::Use_n_Pages(int runlen) {
     runLength = runlen;
 }
 
 void* GB_Thread(void *currentObj) {
-    
-        GroupBy obj = *((GroupBy *) currentObj);
 
-        obj.Operation();
-        pthread_exit(NULL);
+    GroupBy obj = *((GroupBy *) currentObj);
+
+    obj.Operation();
+    pthread_exit(NULL);
 }
 
-void GroupBy::Run (Pipe &inPipe, Pipe &outPipe, OrderMaker &groupAtts, Function &computeMe) {
+void GroupBy::Run(Pipe &inPipe, Pipe &outPipe, OrderMaker &groupAtts, Function &computeMe) {
     g_inPipe = &inPipe;
     g_outPipe = &outPipe;
     g_groupAtts = &groupAtts;
     g_computeMe = &computeMe;
-    
-    int pthreadvar = pthread_create(&thread, NULL, &GB_Thread,(void *) this);
+
+    int pthreadvar = pthread_create(&thread, NULL, &GB_Thread, (void *) this);
     if (pthreadvar) {
-                printf("Error while creating a thread\n");
-                exit(-1);
+        printf("Error while creating a thread\n");
+        exit(-1);
     }
-    
+
 }
 
-void GroupBy::Operation () {
-    /*
+void GroupBy::Operation() {
+
     Pipe sortedOut(PIPE_BUFF_SIZE);
-    
-    BigQ bq(*g_inPipe,sortedOut,*g_groupAtts,runLength);
-    /*
-    pthread_t BigQ_thread;
-    
-    int error = pthread_create(&BigQ_thread, NULL,BigQThread,(void *) &arg);
-        
-        if(error)
-        {
-            cout<<"\n Error while creating the BigQ Thread error# ";
-        }
-     */
-    /*
-    Record rec,recNext;
+
+    //start the BigQ to get the sorted output.
+    BigQ bq(*g_inPipe, sortedOut, *g_groupAtts, runLength);
+
+    Record rec[2], sumResultRec, outRec;
     ComparisonEngine comp;
-    
-    if(sortedOut.Remove(&rec))
-    {
-        while(sortedOut.Remove(&recNext))
-        {
-            if(comp.Compare(&rec,&recNext,&dr_orderMaker)!=0)
-            {
-                dr_outPipe->Insert(&rec);
-                rec=recNext;
-            }
-        }
-        dr_outPipe->Insert(&rec);
+    int intResult, finalIntResult = 0, i;
+    double doubleResult, finalDoubleResult = 0;
+
+    //first we will create the attsToKeep array of attributes to be used in the merge operation.
+    int omAtts = g_groupAtts->numAtts;
+    ; //total attributes in the ordermaker
+    int totalAtts = omAtts + 1; //attributes in output
+
+    int attsToKeep[totalAtts]; //used for the merge operation
+
+    attsToKeep[0] = 0; //first attribute is result of function
+
+    // cop[y rest of the attributes from the ordermaker.
+    for (i = 0; i < omAtts; i++) {
+        attsToKeep[i + 1] = g_groupAtts->whichAtts[i];
     }
-    
-    dr_outPipe->ShutDown();
-    */
-    
+
+    //the type of the result of sum
+    Type resultType = Double;
+
+    //remove the first record and apply the function to it and save the sum.
+    sortedOut.Remove(&rec[0]);
+    resultType = g_computeMe->Apply(rec[0], intResult, doubleResult);
+    if (resultType == Int) {
+        finalIntResult += intResult;
+    } else if (resultType == Double) {
+        finalDoubleResult += doubleResult;
+    }
+
+    //now remove all the next records compare, apply function and insert the new records.
+    i = 1;
+    while (sortedOut.Remove(&rec[i % 2])) {
+
+        //if the records are not equal then first merge and insert the second last removed record in output pipe.
+        if (comp.Compare(&rec[0], &rec[1], g_groupAtts) != 0) {
+
+            if (resultType == Int) {
+                sumResultRec.ComposeRecord(finalIntResult);
+            } else if (resultType == Double) {
+                sumResultRec.ComposeRecord(finalDoubleResult);
+            }
+            //merge record with the record that was inserted second last. and insert it into the output queue.
+            outRec.MergeRecords(&sumResultRec, &rec[(i + 1) % 2], 1, omAtts, attsToKeep, totalAtts, 1);
+            g_outPipe->Insert(&outRec);
+            //reinitialize the the final sum results.
+            finalIntResult = finalDoubleResult = 0;
+
+        }
+        //apply function to newly extracted record.
+        g_computeMe->Apply(rec[i % 2], intResult, doubleResult);
+        if (resultType == Int) {
+            finalIntResult += intResult;
+        } else if (resultType == Double) {
+            finalDoubleResult += doubleResult;
+        }
+        i++;
+    }
+
+    //insert the last record.
+    if (resultType == Int) {
+        sumResultRec.ComposeRecord(finalIntResult);
+    } else if (resultType == Double) {
+        sumResultRec.ComposeRecord(finalDoubleResult);
+    }
+    //merge record with the record that was inserted second last. and insert it into the output queue.
+    //reinitialize the the final sum results.
+    outRec.MergeRecords(&sumResultRec, &rec[(i + 1) % 2], 1, omAtts, attsToKeep, totalAtts, 1);
+    g_outPipe->Insert(&outRec);
+
+    g_outPipe->ShutDown();
+
 }
 
-void GroupBy::WaitUntilDone () {
-	pthread_join (thread, NULL);
+void GroupBy::WaitUntilDone() {
+    pthread_join(thread, NULL);
 }
 
-void GroupBy::Use_n_Pages (int runlen) {
+void GroupBy::Use_n_Pages(int runlen) {
     runLength = runlen;
 }
 
 void* S_Thread(void *currentObj) {
-    
-        Sum obj = *((Sum *) currentObj);
 
-        obj.Operation();
-        pthread_exit(NULL);
+    Sum obj = *((Sum *) currentObj);
+
+    obj.Operation();
+    pthread_exit(NULL);
 }
 
-void Sum::Run (Pipe &inPipe, Pipe &outPipe, Function &computeMe) {
+void Sum::Run(Pipe &inPipe, Pipe &outPipe, Function &computeMe) {
     s_inPipe = &inPipe;
     s_outPipe = &outPipe;
     s_computeMe = &computeMe;
-    
-    int pthreadvar = pthread_create(&thread, NULL, &S_Thread,(void *) this);
+
+    int pthreadvar = pthread_create(&thread, NULL, &S_Thread, (void *) this);
     if (pthreadvar) {
-                printf("Error while creating a thread\n");
-                exit(-1);
+        printf("Error while creating a thread\n");
+        exit(-1);
     }
-    
+
 }
 
-void Sum::Operation () {
-    
-    Record rec,outRec;
-    Type ret=Double; // Initialize to atleast print 0 and not give an error
-    int intResult,finalIntResult=0;
-    double doubleResult,finalDoubleResult=0;
-  
+void Sum::Operation() {
+
+    Record rec, outRec;
+    Type ret = Double; // Initialize to atleast print 0 and not give an error
+    int intResult, finalIntResult = 0;
+    double doubleResult, finalDoubleResult = 0;
+
     //efficiently fine out the sum and save it.
-    if(s_inPipe->Remove(&rec)) {
-        ret = s_computeMe->Apply(rec,intResult,doubleResult);
-        if(ret==Int) {
-            finalIntResult+=intResult;
+    if (s_inPipe->Remove(&rec)) {
+        ret = s_computeMe->Apply(rec, intResult, doubleResult);
+        if (ret == Int) {
+            finalIntResult += intResult;
             while (s_inPipe->Remove(&rec)) {
-                        s_computeMe->Apply(rec,intResult,doubleResult);
-                        finalIntResult+=intResult;
-                }
-        }
-        else if(ret==Double) {
-            finalDoubleResult+=doubleResult;
+                s_computeMe->Apply(rec, intResult, doubleResult);
+                finalIntResult += intResult;
+            }
+        } else if (ret == Double) {
+            finalDoubleResult += doubleResult;
             while (s_inPipe->Remove(&rec)) {
-                        s_computeMe->Apply(rec,intResult,doubleResult);
-                        finalDoubleResult+=doubleResult;
-                }
-        }
-        else {
-            cout<<"Wront type returned by Apply function in Function. Error called from Sum::Operation";
+                s_computeMe->Apply(rec, intResult, doubleResult);
+                finalDoubleResult += doubleResult;
+            }
+        } else {
+            cout << "Wront type returned by Apply function in Function. Error called from Sum::Operation";
         }
     }
     /*
@@ -394,25 +430,59 @@ void Sum::Operation () {
         const char*src = str.c_str();
         outRec.ComposeRecord(&mySchema, src);
         remove(name);
-        */
-    
-        //Compuse a new record based on the integer or double result.
-        if (ret == Int) {
-            outRec.ComposeRecord(finalIntResult);
-        } else if (ret == Double){
-            outRec.ComposeRecord(finalDoubleResult);
-        }
-        
-        //insert the new record in the output pipe and 
-        s_outPipe->Insert(&outRec);
-        s_outPipe->ShutDown();
-        
+     */
+
+    //Compuse a new record based on the integer or double result.
+    if (ret == Int) {
+        outRec.ComposeRecord(finalIntResult);
+    } else if (ret == Double) {
+        outRec.ComposeRecord(finalDoubleResult);
+    }
+
+    //insert the new record in the output pipe and 
+    s_outPipe->Insert(&outRec);
+    s_outPipe->ShutDown();
+
 }
 
-void Sum::WaitUntilDone () {
-	pthread_join (thread, NULL);
+void Sum::WaitUntilDone() {
+    pthread_join(thread, NULL);
 }
 
-void Sum::Use_n_Pages (int runlen) {
+void Sum::Use_n_Pages(int runlen) {
+    runLength = runlen;
+}
+
+void* J_Thread(void *currentObj) {
+
+    Join obj = *((Join *) currentObj);
+
+    obj.Operation();
+    pthread_exit(NULL);
+}
+
+void Join::Run(Pipe &inPipeL, Pipe &inPipeR, Pipe &outPipe, CNF &selOp, Record &literal) {
+    j_inPipeL = &inPipeL;
+    j_inPipeR = &inPipeR;
+    j_outPipe = &outPipe;
+    j_selOp = &selOp;
+    j_literal = &literal;
+
+    int pthreadvar = pthread_create(&thread, NULL, &J_Thread, (void *) this);
+    if (pthreadvar) {
+        printf("Error while creating a thread\n");
+        exit(-1);
+    }
+}
+
+void Join::Operation() {
+
+}
+
+void Join::WaitUntilDone() {
+    pthread_join(thread, NULL);
+}
+
+void Join::Use_n_Pages(int runlen) {
     runLength = runlen;
 }
--- project3/RelOp.h
+++ project3/RelOp.h
@@ -9,111 +9,121 @@
 class RelationalOp {
 protected:
     pthread_t thread;
-	public:
-	// blocks the caller until the particular relational operator 
-	// has run to completion
-	virtual void WaitUntilDone () = 0;
-        
-        virtual void Operation() {}; // make it =0; after all functions are implemented
-        
-        virtual void Run() {}; // make it =0; after all functions are implemented
-        
-        virtual void Use_n_Pages (int n) = 0;
+public:
+    // blocks the caller until the particular relational operator 
+    // has run to completion
+    virtual void WaitUntilDone() = 0;
+
+    virtual void Operation() = 0; // make it =0; after all functions are implemented
+
+    virtual void Run() {}; // make it =0; after all functions are implemented
+
+    virtual void Use_n_Pages(int n) = 0;
 };
 
-class SelectFile : public RelationalOp { 
+class SelectFile : public RelationalOp {
+private:
+    DBFile *sf_inputFile;
+    Pipe *sf_outputPipe;
+    CNF *sf_selectOperator;
+    Record *sf_literalRecord;
+    int runLength;
 
-	private:
-        DBFile *sf_inputFile;
-        Pipe *sf_outputPipe;
-        CNF *sf_selectOperator;
-        Record *sf_literalRecord;
-        int runLength;
-	        
-	public:
+public:
 
-	void Run (DBFile &inFile, Pipe &outPipe, CNF &selOp, Record &literal);
-        void Operation();
-	void WaitUntilDone ();
-	void Use_n_Pages (int n);
+    void Run(DBFile &inFile, Pipe &outPipe, CNF &selOp, Record &literal);
+    void Operation();
+    void WaitUntilDone();
+    void Use_n_Pages(int n);
 
 };
 
 class SelectPipe : public RelationalOp {
 private:
-    Pipe *sp_inPipe,*sp_outPipe;
+    Pipe *sp_inPipe, *sp_outPipe;
     CNF *sp_selOp;
     Record *sp_literal;
     int runLength;
-	public:
-	void Run (Pipe &inPipe, Pipe &outPipe, CNF &selOp, Record &literal);
-	void WaitUntilDone ();
-        void Operation();
-        void Use_n_Pages (int n);
+public:
+    void Run(Pipe &inPipe, Pipe &outPipe, CNF &selOp, Record &literal);
+    void WaitUntilDone();
+    void Operation();
+    void Use_n_Pages(int n);
 };
-class Project : public RelationalOp { 
+
+class Project : public RelationalOp {
 private:
-    Pipe *p_inPipe,*p_outPipe;
-    int *p_keepMe,p_numAttsInput,p_numAttsOutput;
+    Pipe *p_inPipe, *p_outPipe;
+    int *p_keepMe, p_numAttsInput, p_numAttsOutput;
     int runLength;
-    
-    
-	public:
-        void Operation();
-	void Run (Pipe &inPipe, Pipe &outPipe, int *keepMe, int numAttsInput, int numAttsOutput);
-	void WaitUntilDone ();
-	void Use_n_Pages (int n);
+
+public:
+    void Operation();
+    void Run(Pipe &inPipe, Pipe &outPipe, int *keepMe, int numAttsInput, int numAttsOutput);
+    void WaitUntilDone();
+    void Use_n_Pages(int n);
 };
-class Join : public RelationalOp { 
-	public:
-	void Run (Pipe &inPipeL, Pipe &inPipeR, Pipe &outPipe, CNF &selOp, Record &literal) { }
-	void WaitUntilDone () { }
-	void Use_n_Pages (int n) { }
+
+class Join : public RelationalOp {
+private:
+    Pipe *j_inPipeL, *j_inPipeR, *j_outPipe;
+    CNF *j_selOp;
+    Record *j_literal;
+    int runLength;
+public:
+    void Run(Pipe &inPipeL, Pipe &inPipeR, Pipe &outPipe, CNF &selOp, Record &literal);
+    void WaitUntilDone();
+    void Use_n_Pages(int n);
+    void Operation();
 };
+
 class DuplicateRemoval : public RelationalOp {
 private:
-    Pipe *dr_inPipe,*dr_outPipe;
+    Pipe *dr_inPipe, *dr_outPipe;
     Schema *dr_mySchema;
     int runLength;
-	public:
-	void Run (Pipe &inPipe, Pipe &outPipe, Schema &mySchema);
-	void WaitUntilDone ();
-	void Use_n_Pages (int n);
-        void Operation();
+public:
+    void Run(Pipe &inPipe, Pipe &outPipe, Schema &mySchema);
+    void WaitUntilDone();
+    void Use_n_Pages(int n);
+    void Operation();
 };
+
 class Sum : public RelationalOp {
 private:
-    Pipe *s_inPipe,*s_outPipe;
+    Pipe *s_inPipe, *s_outPipe;
     Function *s_computeMe;
     int runLength;
-	public:
-	void Run (Pipe &inPipe, Pipe &outPipe, Function &computeMe);
-	void WaitUntilDone ();
-	void Use_n_Pages (int n);
-        void Operation();
+public:
+    void Run(Pipe &inPipe, Pipe &outPipe, Function &computeMe);
+    void WaitUntilDone();
+    void Use_n_Pages(int n);
+    void Operation();
 };
+
 class GroupBy : public RelationalOp {
 private:
-    Pipe *g_inPipe,*g_outPipe;
+    Pipe *g_inPipe, *g_outPipe;
     OrderMaker *g_groupAtts;
     Function *g_computeMe;
     int runLength;
-	public:
-	void Run (Pipe &inPipe, Pipe &outPipe, OrderMaker &groupAtts, Function &computeMe);
-	void WaitUntilDone ();
-	void Use_n_Pages (int n);
-        void Operation();
+public:
+    void Run(Pipe &inPipe, Pipe &outPipe, OrderMaker &groupAtts, Function &computeMe);
+    void WaitUntilDone();
+    void Use_n_Pages(int n);
+    void Operation();
 };
+
 class WriteOut : public RelationalOp {
 private:
     Pipe *wo_inPipe;
     FILE *wo_outFile;
     Schema *wo_mySchema;
     int runLength;
-	public:
-	void Run (Pipe &inPipe, FILE *outFile, Schema &mySchema);
-	void WaitUntilDone ();
-        void Operation ();
-        void Use_n_Pages (int n);
+public:
+    void Run(Pipe &inPipe, FILE *outFile, Schema &mySchema);
+    void WaitUntilDone();
+    void Operation();
+    void Use_n_Pages(int n);
 };
 #endif
